bec05e5d9469798b1029ebad3cbe79413b877438
Wilkin Vásquez <wilkinvasquez@gmail.com>
2016-12-31 13:31:59 -0600

    Still working on Main section.

---
+++ Taskoo.Tests/Controllers/MainControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taskoo.Controllers;
+
+namespace Taskoo.Tests.Controllers
+{
+    [TestClass]
+    public class MainControllerTest
+    {
+        [TestMethod]
+        public void FindPriorityIconName_MustReturnPriorityIconName()
+        {
+            // Arrange
+            MainController controller1 = new MainController();
+            MainController controller2 = new MainController();
+            MainController controller3 = new MainController();
+
+            // Act
+            string result1 = controller1.findPriorityIconName('r');
+            string result2 = controller2.findPriorityIconName('g');
+            string result3 = controller3.findPriorityIconName('y');
+
+            // Assert
+            Assert.AreEqual("red-triangle", result1);
+            Assert.AreEqual("green-triangle", result2);
+            Assert.AreEqual("yellow-triangle", result3);
+        }
+
+        [TestMethod]
+        public void FindIsFinishedIconName_MustReturnIsFinishedIconName()
+        {
+            // Arrange
+            MainController controller1 = new MainController();
+            MainController controller2 = new MainController();
+
+            // Act
+            string result1 = controller1.findIsFinishedIconName(false);
+            string result2 = controller2.findIsFinishedIconName(true);
+
+            // Assert
+            Assert.AreEqual("check1", result1);
+            Assert.AreEqual("check2", result2);
+        }
+    }
+}
--- Taskoo/Controllers/MainController.cs
+++ Taskoo/Controllers/MainController.cs
@@ -1,28 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Threading;
-using System.Web;
 using System.Web.Mvc;
+using Taskoo.Helpers;
 
 namespace Taskoo.Controllers
 {
     public class MainController : Controller
     {
         // GET: Main
-        public ActionResult Index()
+        public ActionResult Index(char? id)
         {
             TaskDataContext db = new TaskDataContext();
 
             var query = from c in db.Tasks
                         select c;
 
-            var tasks = query.ToList();
+            switch (id)
+            {
+                case 'r':
+                    query = from c in db.Tasks
+                            where c.priority == 'r'
+                            select c;
+                    break;
+                case 'g':
+                    query = from c in db.Tasks
+                            where c.priority == 'g'
+                            select c;
+                    break;
+                case 'y':
+                    query = from c in db.Tasks
+                            where c.priority == 'y'
+                            select c;
+                    break;
+            }
 
-            ViewBag.Tasks = tasks;
+            List<Task> tasks = query.ToList();
+
+            ViewBag.Tasks = ProcessTasks(tasks);
 
             return View();
+        }
+
+        // No test for this method yet.
+        public List<TaskProcessed> ProcessTasks(List<Task> tasks)
+        {
+            List<TaskProcessed> tasksProcessedList = new List<TaskProcessed>();
+
+            foreach (var task in tasks)
+            {
+                TaskProcessed taskProcessed = new TaskProcessed();
+
+                taskProcessed.Priority = (char) task.priority;
+                taskProcessed.PriorityIconName = findPriorityIconName((char) task.priority);
+                taskProcessed.IsFinishedIconName = findIsFinishedIconName((bool) task.isDone);
+                taskProcessed.Title = task.title;
+                taskProcessed.Date = String.Format("{0:dddd, MMMM d}", task.finalDate);
+                taskProcessed.Time = String.Format("{0:T}", task.finalDate);
+                taskProcessed.Description = task.description;
+
+                tasksProcessedList.Add(taskProcessed);
+            }
+
+            return tasksProcessedList;
+        }
+
+        public string findPriorityIconName(char priority)
+        {
+            string priorityIconName = "";
+
+            switch (priority)
+            {
+                case 'r':
+                    priorityIconName = "red-triangle";
+                    break;
+                case 'y':
+                    priorityIconName = "yellow-triangle";
+                    break;
+                case 'g':
+                    priorityIconName = "green-triangle";
+                    break;
+            }
+
+            return priorityIconName;
+        }
+
+        public string findIsFinishedIconName(bool isDone)
+        {
+            return isDone ? "check2" : "check1";
         }
     }
 }
