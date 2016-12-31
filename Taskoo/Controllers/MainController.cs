d7772f6c38ae0597e99c8b5f614bb940831bf2b9
Wilkin Vásquez <wilkinvasquez@gmail.com>
2016-12-30 22:51:35 -0600

    Working on Main section.

--- Taskoo/Controllers/MainController.cs
+++ Taskoo/Controllers/MainController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,15 @@ namespace Taskoo.Controllers
         // GET: Main
         public ActionResult Index()
         {
+            TaskDataContext db = new TaskDataContext();
+
+            var query = from c in db.Tasks
+                        select c;
+
+            var tasks = query.ToList();
+
+            ViewBag.Tasks = tasks;
+
             return View();
         }
     }
